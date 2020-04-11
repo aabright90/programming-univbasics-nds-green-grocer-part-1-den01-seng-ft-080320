63e9d687bb0fc106ce371e739a9e93c06045ba8d
Austen Bright <pureaustenbright@gmail.com>
2020-04-11 20:48:46 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -19,7 +19,7 @@ def consolidate_cart(cart)
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
   final_cart = {}
   cart.each do |item|
-    if find_item_by_name_in_collection(item, cart)
+    
     binding.pry
     end
   end
