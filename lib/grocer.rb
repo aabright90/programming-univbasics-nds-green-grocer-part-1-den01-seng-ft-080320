5bb3b9de16a44082a8b79d09bd9c7047dae4fa3b
Austen Bright <pureaustenbright@gmail.com>
2020-04-11 20:34:27 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -6,7 +6,7 @@ def find_item_by_name_in_collection(name, collection)
   collection.each do |item|
     if name == item[:item]
     # binding.pry
-    item[:item]
+    return item[:item]
     end
   end
 end
